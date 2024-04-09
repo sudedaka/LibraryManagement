ea2a7de9048d964a4588e87862f574f2d584a90c
DogaOrhan <dogaorhan6@gmail.com>
2024-04-09 14:39:17 +0300

    Import, export buttons integrated

--- src/main/java/com/example/librarymanagement/MainWindowController.java
+++ src/main/java/com/example/librarymanagement/MainWindowController.java
@@ -8,14 +8,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import com.google.gson.Gson;
+
+import java.io.*;
 
 
 import java.io.File;
@@ -31,6 +35,8 @@ import javafx.fxml.FXML;
 
 
 public class MainWindowController extends Application {
+
+
     @FXML
     private ArrayList<Book> books;
     @FXML
@@ -47,10 +53,15 @@ public class MainWindowController extends Application {
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
+
     }
+
     public static void main(String[] args) {
+
+
         launch();
     }
+
 
     @FXML
     public void addButtonClick(ActionEvent event) throws IOException
@@ -64,13 +75,15 @@ public class MainWindowController extends Application {
         stage.showAndWait();
     }
 
+
     @FXML
-    public void importFromJson(String filePath) {
+
+    public void importFromJson(String filePath) {   // Method to read data from a JSON file
         try {
             Gson gson = new Gson();
             try (Reader reader = new FileReader(filePath)) {
                 books = gson.fromJson(reader, new TypeToken<ArrayList<Book>>() {
-                }.getType());
+                }.getType());  // Add data read from JSON file to the books list
                 System.out.println("Books imported from JSON .");
             } catch (IOException e) {
                 e.printStackTrace();
@@ -81,6 +94,48 @@ public class MainWindowController extends Application {
             throw new RuntimeException(e);
         }
     }
+    @FXML
+
+    public void importButton(ActionEvent event) throws IOException
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select JSON File");
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            importFromJson(file.getAbsolutePath());  // Read data from the file
+        }
+    }
+    @FXML
+    public void exportToJson(String path) {
+        try {
+            Gson gson = new Gson();
+            String jsonFormat = gson.toJson(books);
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+                writer.write(jsonFormat);
+            } catch (IOException e) {
+                System.err.println("An error occurred while writing to the file: " + e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+    @FXML
+    private void exportButton(ActionEvent event) {
+
+        Gson gson = new Gson();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FileChooser file = new FileChooser();
+        file.setInitialFileName("library.json");
+        FileChooser.ExtensionFilter fileExtensions = new FileChooser.ExtensionFilter("File", "*.json");
+        file.getExtensionFilters().add(fileExtensions);
+        file.setTitle("Choose to save!");
+        File f = file.showSaveDialog(stage);
+        exportToJson(f.getAbsolutePath());   // Write data in JSON format to the selected file
+    }
+
+
 
     //COVER IMPORT BUTTON FUNCTION RUNNING
     @FXML
@@ -94,3 +149,4 @@ public class MainWindowController extends Application {
         }
     }
 }
+
